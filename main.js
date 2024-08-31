6312e6b16dd3d7c3435886d14e84811d6cbaa93d
Sholastix <drenai6436@gmail.com>
2024-08-31 18:44:23 +0300

    Custom menu added.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Check if we in development mode.
 const isDevMode = process.env.NODE_ENV !== 'production';
@@ -9,6 +9,7 @@ const isMacOS = process.platform === 'darwin';
 // function that creates a window.
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
+    // titleBarStyle: "hidden",
     title: "ImageReziser",
     width: 720,
     height: 480
@@ -19,12 +20,21 @@ const createMainWindow = () => {
     mainWindow.webContents.openDevTools();
   };
 
+  // Load file that will be opened in 'electron' window.
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 };
 
+// Launches when app is ready.
 app.on('ready', () => {
   createMainWindow();
 
+  // Custom window menu implementation.
+  const customMenu = Menu.buildFromTemplate(menu);
+
+  // Connect custom menu to app.
+  Menu.setApplicationMenu(customMenu);
+
+  // This part for MacOS.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
@@ -32,7 +42,21 @@ app.on('ready', () => {
   });
 });
 
-// This part just for MacOS.
+// Custom window menu template.
+const menu = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: () => app.quit(),
+        accelerator: 'Ctrl+Q'
+      },
+    ]
+  },
+]
+
+// This part for MacOS.
 app.on('window-all-closed', () => {
   if (!isMacOS) {
     app.quit()
